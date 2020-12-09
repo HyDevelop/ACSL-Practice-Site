7a4ed3db110dabbd29884c622c6e6fcd8bfa0373
Hykilpikonna <me@hydev.org>
2020-12-09 14:15:16 -0500

    [+] Create question text

--- src/views/1/NumberingSystem.vue
+++ src/views/1/NumberingSystem.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="question">
         <h1>Numbering Systems Practice</h1>
+
+        <div class="label">Question: </div>
+        <div class="question-text">{{question}}</div>
+
     </div>
 </template>
 
@@ -10,6 +14,10 @@ import {randInt} from "@/scripts/utils";
 
 export default class NumberingSystem extends Vue
 {
+    question = "";
+
+    fromBases = "2, 8, 10, 16";
+    toBases = "2, 8, 10, 16";
 
 }
 </script>
