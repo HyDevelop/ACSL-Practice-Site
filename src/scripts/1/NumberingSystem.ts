0b92aaad378f50f2bb15b159e36e286f9368758b
Hykilpikonna <me@hydev.org>
2020-12-11 14:33:43 -0500

    [+] Encapsulate rand([])

--- src/scripts/1/NumberingSystem.ts
+++ src/scripts/1/NumberingSystem.ts
@@ -1,5 +1,6 @@
 import Problem from "@/scripts/Problem";
-import {randInt} from "@/scripts/utils";
+import {rand, randInt} from "@/scripts/utils";
+import {getRandomInt} from "element-plus/lib/utils/util";
 
 export default class NumberingSystem extends Problem
 {
@@ -18,10 +19,8 @@ export default class NumberingSystem extends Problem
         // Generate target number
         this.n = randInt(+this.options[2].value, +this.options[3].value)
 
-        const fb = this.options[0].value.split(',').map(it => +it);
-        const tb = this.options[1].value.split(',').map(it => +it);
-        const fromBase = fb[Math.floor(Math.random() * fb.length)]
-        const toBase = tb[Math.floor(Math.random() * tb.length)]
+        const fromBase = +rand(this.options[0].value.split(','))
+        const toBase = +rand(this.options[1].value.split(','))
 
         const fromText = this.n.toString(fromBase).toUpperCase()
         const toText = this.n.toString(toBase).toUpperCase()
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -10,3 +10,8 @@ export function randBool()
 {
     return Math.random() < 0.5
 }
+
+export function rand<T>(a: T[])
+{
+    return a[Math.floor(Math.random() * a.length)]
+}
